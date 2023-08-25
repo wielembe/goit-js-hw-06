348b9e35b0f76af789a0151e3d487c65a09672ad
wielembe <e.wielemborek@gmail.com>
2023-08-25 20:01:33 +0200

    Update task-01.js
    test  upload

---
+++ js/task-01.js
@@ -0,0 +1,16 @@
+"use strict";
+const listCategories = document.querySelectorAll("#categories li.item");
+const amountCategories = listCategories.length;
+
+console.log(`Number of categories: ${amountCategories}`);
+
+listCategories.forEach((category) => {
+    const titleCategory = category.querySelector("h2");
+    const nameCategory = titleCategory.textContent;
+
+    const listElements = category.querySelectorAll("ul li");
+    const amountElements = listElements.length;
+
+    console.log(`Category: ${nameCategory}`);
+    console.log(`Elements: ${amountElements}`);
+});
