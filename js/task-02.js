8275aa8f737547dc8adab83f9f6644f0d76b17b3
wielembe <e.wielemborek@gmail.com>
2023-08-26 19:53:01 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,42 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
 ];
+
+const listIngredients = document.querySelector("#ingredients");
+const firstItem = document.createElement("li");
+firstItem.textContent = ingredients[0];
+firstItem.classList.add("item");
+
+const secondItem = document.createElement("li");
+secondItem.textContent = ingredients[1];
+secondItem.classList.add("item");
+
+const thirdItem = document.createElement("li");
+thirdItem.textContent = ingredients[2];
+thirdItem.classList.add("item");
+
+const forthItem = document.createElement("li");
+forthItem.textContent = ingredients[3];
+forthItem.classList.add("item");
+
+const fifthItem = document.createElement("li");
+fifthItem.textContent = ingredients[4];
+fifthItem.classList.add("item");
+
+const sixthItem = document.createElement("li");
+sixthItem.textContent = ingredients[5];
+sixthItem.classList.add("item");
+
+listIngredients.append(
+    firstItem,
+    secondItem,
+    thirdItem,
+    forthItem,
+    fifthItem,
+    sixthItem
+);
