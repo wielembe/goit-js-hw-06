c7696177f3ff2453d219745b8ba9fe0f0763407b
wielembe <e.wielemborek@gmail.com>
2023-08-28 20:42:47 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,4 +1,3 @@
-"use strict";
 const ingredients = [
     "Potatoes",
     "Mushrooms",
@@ -8,36 +7,11 @@ const ingredients = [
     "Condiments",
 ];
 
-const listIngredients = document.querySelector("#ingredients");
-const firstItem = document.createElement("li");
-firstItem.textContent = ingredients[0];
-firstItem.classList.add("item");
+const ingredientsList = document.querySelector("#ingredients");
 
-const secondItem = document.createElement("li");
-secondItem.textContent = ingredients[1];
-secondItem.classList.add("item");
-
-const thirdItem = document.createElement("li");
-thirdItem.textContent = ingredients[2];
-thirdItem.classList.add("item");
-
-const forthItem = document.createElement("li");
-forthItem.textContent = ingredients[3];
-forthItem.classList.add("item");
-
-const fifthItem = document.createElement("li");
-fifthItem.textContent = ingredients[4];
-fifthItem.classList.add("item");
-
-const sixthItem = document.createElement("li");
-sixthItem.textContent = ingredients[5];
-sixthItem.classList.add("item");
-
-listIngredients.append(
-    firstItem,
-    secondItem,
-    thirdItem,
-    forthItem,
-    fifthItem,
-    sixthItem
-);
+ingredients.forEach((ingredient) => {
+    const listElement = document.createElement("li");
+    listElement.textContent = ingredient;
+    listElement.className = "item";
+    ingredientsList.append(listElement);
+});
