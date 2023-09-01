6ef858f4ff00b5b5aac8273334635fc36a9db3fb
wielembe <e.wielemborek@gmail.com>
2023-09-02 01:28:04 +0200

    more remarks

--- js/task-02.js
+++ js/task-02.js
@@ -9,6 +9,7 @@ const ingredients = [
 
 const ingredientsList = document.querySelector("#ingredients");
 
+//1. stary kod
 // ingredients.forEach((ingredient) => {
 //     const listElement = document.createElement("li");
 //     listElement.textContent = ingredient;
@@ -16,6 +17,20 @@ const ingredientsList = document.querySelector("#ingredients");
 //     ingredientsList.append(listElement);
 // });
 
-const ingredientsArr = ingredients.map((ingredient) => ingredient);
+//2. mapa i join
+// const ingredientsArr = ingredients
+//     .map((ingredient) => `<li class= "item">${ingredient}</li>`)
+//     .join("");
+// ingredientsList.insertAdjacentHTML("beforeend", ingredientsArr);
 
-console.log(ingredientsArr);
+//3. Jeśli nie tak.. to już nie wiem
+
+const ingredientsArr = ingredients.map((ingredient) => {
+    const listElement = document.createElement("li");
+    listElement.textContent = ingredient;
+
+    listElement.className = "item";
+    return listElement;
+});
+
+ingredientsList.append(...ingredientsArr);
