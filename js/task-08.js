0ab6950b8f289390297b4423251d10f666ae61cc
wielembe <e.wielemborek@gmail.com>
2023-09-01 16:12:41 +0200

    8-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
+const loginData = {};
 
+const submit = (onClick) => {
+    onClick.preventDefault();
+
+    const {
+        elements: { email, password },
+    } = onClick.currentTarget;
+    if (email.value === "" || password.value === "") {
+        return alert("Uzupełnij wszystkie pola!");
+    }
+    loginData.email = email.value;
+    loginData.password = password.value;
+
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    onClick.currentTarget.reset();
+    console.log(loginData);
+};
+
+form.addEventListener("submit", submit);
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const backgroundColor = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+changeColorButton.addEventListener("click", () => {
+    let currentColor = getRandomHexColor();
+
+    body.style.backgroundColor = `${currentColor}`;
+    backgroundColor.textContent = `${currentColor}`;
+});
